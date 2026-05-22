d69ce33f63b4a33e49cfed425541587f6de464d7
Vincent LAURENT <vince.laurent@gmail.com>
2026-05-22 21:58:51 +0200

    Enregistrement du fichier brut

--- bin/get.sh
+++ bin/get.sh
@@ -1,6 +1,8 @@
 export LC_ALL=fr_FR.UTF-8
 
-curl https://docs.google.com/document/d/1sh-xkEMkNLGw7U8GacAPe4p6828jnDVApuIVeYb8VnI/mobilebasic | grep -v "DOCTYPE" | tr -d "\n" | sed 's|</p>|#|g' | sed -r 's|<style>.*</style>||g' | sed -r 's|<javascript>.*</javascript>||g'  | sed -r 's|</?[a-z]+[^<>]*>||g' | sed -r 's/^.*zapperbollore@proton\.me//' | tr '#' "\n" | grep "," | grep -v window.set | grep -v function | sed 's/^39;//' | sed -r 's/[ \t]+$//' | sed -r 's/[ ]+/ /g' | sort > /tmp/liste_complete.txt.tmp
+curl https://docs.google.com/document/d/1sh-xkEMkNLGw7U8GacAPe4p6828jnDVApuIVeYb8VnI/mobilebasic > liste_complete.html
+
+cat liste_complete.html | grep -v "DOCTYPE" | tr -d "\n" | sed 's|</p>|#|g' | sed -r 's|<style>.*</style>||g' | sed -r 's|<javascript>.*</javascript>||g'  | sed -r 's|</?[a-z]+[^<>]*>||g' | sed -r 's/^.*zapperbollore@proton\.me//' | tr '#' "\n" | grep "," | grep -v window.set | grep -v function | sed 's/^39;//' | sed -r 's/[ \t]+$//' | sed -r 's/[ ]+/ /g' | sort > /tmp/liste_complete.txt.tmp
 
 if [ "$(md5sum /tmp/liste_complete.txt.tmp | cut -d " " -f 1)" != "$(md5sum liste_complete.txt | cut -d " " -f 1)" ]
 then
@@ -11,5 +13,5 @@ fi
 
 bash bin/stats.sh
 
-git add liste_complete.txt README.md
-git commit liste_complete.txt README.md -m "Mise à jour de la liste"
+git add liste_complete.html liste_complete.txt README.md
+git commit liste_complete.html liste_complete.txt README.md -m "Mise à jour de la liste"
